82489e77b65f94e3035b6be9c8045a8f231c6349
Eemeli Ponkilainen <111982474+EemelPo@users.noreply.github.com>
2025-04-06 20:16:16 +0300

    Update eventmanagementsql.sql

--- eventmanagementsql.sql
+++ eventmanagementsql.sql
@@ -1,28 +1,26 @@
--- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: eventmanagementtest
--- ------------------------------------------------------
--- Server version	10.11.6-MariaDB-0+deb12u1
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               11.7.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.10.0.7000
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `attendance`
---
 
-DROP TABLE IF EXISTS `attendance`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `attendance` (
+-- Dumping database structure for eventmanagement
+CREATE DATABASE IF NOT EXISTS `eventmanagement` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_uca1400_ai_ci */;
+USE `eventmanagement`;
+
+-- Dumping structure for table eventmanagement.attendance
+CREATE TABLE IF NOT EXISTS `attendance` (
   `attendanceid` int(11) NOT NULL AUTO_INCREMENT,
   `EventId` int(11) NOT NULL,
   `UserId` int(11) NOT NULL,
@@ -31,26 +29,12 @@ CREATE TABLE `attendance` (
   KEY `UserId` (`UserId`),
   CONSTRAINT `FKc0dhmodtc2c23usnfrrc03hdk` FOREIGN KEY (`EventId`) REFERENCES `event` (`eventid`),
   CONSTRAINT `FKtajpmxntt2g3j6496h4glhg3i` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `attendance`
---
+-- Data exporting was unselected.
 
-LOCK TABLES `attendance` WRITE;
-/*!40000 ALTER TABLE `attendance` DISABLE KEYS */;
-/*!40000 ALTER TABLE `attendance` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `event`
---
-
-DROP TABLE IF EXISTS `event`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `event` (
+-- Dumping structure for table eventmanagement.event
+CREATE TABLE IF NOT EXISTS `event` (
   `Date` date NOT NULL,
   `startTime` time(6) NOT NULL,
   `endTime` time(6) NOT NULL,
@@ -62,32 +46,16 @@ CREATE TABLE `event` (
   `MaxAttendance` int(11) NOT NULL,
   `creatorid` int(11) NOT NULL,
   `description` varchar(255) NOT NULL,
+  `language` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`eventid`),
   KEY `FKicjes6nxn9jsgam061mcstoun` (`creatorid`),
   CONSTRAINT `FKicjes6nxn9jsgam061mcstoun` FOREIGN KEY (`creatorid`) REFERENCES `user` (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=116 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `event`
---
+-- Data exporting was unselected.
 
-LOCK TABLES `event` WRITE;
-/*!40000 ALTER TABLE `event` DISABLE KEYS */;
-INSERT INTO `event` VALUES
-('2025-03-12','10:00:00.000000','14:00:00.000000','Spring Festival',1,'Central Park','Festival',100,500,20,'A fun-filled spring celebration with food, music, and games!'),
-('2025-03-12','12:00:00.000000','13:00:00.000000','Cool AI',2,'Myllypuro',NULL,0,0,20,'Testing');
-/*!40000 ALTER TABLE `event` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+-- Dumping structure for table eventmanagement.user
+CREATE TABLE IF NOT EXISTS `user` (
   `FirstName` varchar(255) NOT NULL,
   `LastName` varchar(255) NOT NULL,
   `Email` varchar(255) NOT NULL,
@@ -97,30 +65,12 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=233 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `user`
---
+-- Data exporting was unselected.
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES
-('John','Doe','john.doe@example.com','1234567890',17,0,'johndoe','securePassword'),
-('Test','User','testuser@example.com','1234567890',18,0,'testuser','$2a$12$YYZgzboF35ib9Na3uExcJuDSnX/l1yg1sVmvFrlat8SEWPGl7lW1W'),
-('Juho','User','testuser@example.com','1234567890',19,0,'JuhoS','$2a$12$wp5xnzXzr4ZpTSz63PhYEefy4XmNfLFedNBisuNZiZQSmaZrX4DIu'),
-('Juho','Simpanen','juho.simpanen@metropolia.fi','434563567',20,1,'JuhooS','$2a$12$rKYOGpkAqMeUq/1iTWFViedzWxhi9RR/cWEZrCHGgl3nR70K6r/.S');
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-03-12 13:14:56
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
